4011d1b7d54ce3a5c8c71915cb6b40e85bba1c4a
Saksham Boldhan <sakshamboldhan9@gmail.com>
2026-05-27 08:42:36 +0530

    feat: improve responsiveness of home page buttons

--- src/assets/components/Home/HomeBottomText.jsx
+++ src/assets/components/Home/HomeBottomText.jsx
@@ -4,15 +4,15 @@ import { Link, useNavigate } from 'react-router-dom'
 const HomeBottomText = () => {
 
   return (
-    <div className='font-[font2] flex items-center text-amber-50 justify-center gap-4 pb-14'>
-      <div className='lg:border-2 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-36 h-24 flex items-center px-10 lg:px-16 border-white rounded-full uppercase transition-all duration-300'>
-        <Link className='text-[7vw] lg:text-[4.5vw]' to='/projects'>Projects</Link>
+    <div className='font-[font2] flex flex-wrap items-center text-amber-50 justify-center gap-3 sm:gap-4 pb-14 px-4'>
+      <div className='border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] h-16 sm:h-24 lg:h-36 flex items-center px-6 sm:px-10 lg:px-16 border-white rounded-full uppercase transition-all duration-300'>
+        <Link className='text-[8vw] sm:text-[7vw] lg:text-[4.5vw]' to='/projects'>Projects</Link>
       </div>
-      <div className='lg:border-2 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-36 h-24 flex items-center px-10 lg:px-16 border-white rounded-full uppercase transition-all duration-300'>
-        <Link className='text-[7vw] lg:text-[4.5vw]' to='/agence'>agence</Link>
+      <div className='border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] h-16 sm:h-24 lg:h-36 flex items-center px-6 sm:px-10 lg:px-16 border-white rounded-full uppercase transition-all duration-300'>
+        <Link className='text-[8vw] sm:text-[7vw] lg:text-[4.5vw]' to='/agence'>agence</Link>
       </div>
-      <div className='lg:border-2 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-36 h-24 flex items-center px-10 lg:px-16 border-white rounded-full uppercase transition-all duration-300'>
-        <Link className='text-[7vw] lg:text-[4.5vw]' to='/clients'>Team</Link>
+      <div className='border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] h-16 sm:h-24 lg:h-36 flex items-center px-6 sm:px-10 lg:px-16 border-white rounded-full uppercase transition-all duration-300'>
+        <Link className='text-[8vw] sm:text-[7vw] lg:text-[4.5vw]' to='/clients'>Team</Link>
       </div>
     </div>
   )
